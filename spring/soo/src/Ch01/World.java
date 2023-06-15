77b5ca2689462c6f7281916d17c8fea11c8259fc
oraman <oraman@naver.com>
2023-06-15 17:12:39 +0900

    push 테스트입니다~~

--- spring/soo/src/Ch01/World.java
+++ spring/soo/src/Ch01/World.java
@@ -3,5 +3,8 @@ package Ch01;
 public class World {
     public static void main(String[] args) {
         System.out.println("world01");
+        System.out.println("world02");
+        System.out.println("world03");
+        System.out.println("world04");
     }
 }
