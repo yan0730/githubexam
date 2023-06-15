310bedac0f773149a9cc0b092b5418ed4b340a36
oraman <oraman@naver.com>
2023-06-15 17:32:34 +0900

    테스트 또~

--- spring/soo/src/Ch02/Welcome.java
+++ spring/soo/src/Ch02/Welcome.java
@@ -4,5 +4,6 @@ public class Welcome {
     public static void main(String[] args) {
         System.out.println("welcome01");
         System.out.println("welcome02");
+        System.out.println("welcome03");
     }
 }
